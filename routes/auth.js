039153c4844fc049a1d1cc6a000bd0993cefbbf7
hamzawalaqadr <mh.hamza01@gmail.com>
2023-09-30 13:22:01 +0500

    Added Password encryption to Update API

--- routes/auth.js
+++ routes/auth.js
@@ -42,7 +42,7 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username, isDeleted: false });
-  if (!user) return res.status(400).send('Invalid username or user is deleted');
+  if (!user) return res.status(400).send('Invalid username');
 
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) return res.status(400).send('Invalid password');
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,8 +4,8 @@ const verifyToken = require('../middleware/verifyToken');
 
 const router = express.Router();
 
-router.get('/', verifyToken, async (req, res) => {
-  const users = await User.find().select('-__v -password');
+router.get('/', async (req, res) => {
+  const users = await User.find().select('-__v');
   res.json({ message: 'Displaying users', users });
 });
 
@@ -16,15 +16,36 @@ router.get('/:id', verifyToken, async (req, res) => {
 });
 
 router.put('/:id', verifyToken, async (req, res) => {
-  const { username, password, email, contactNumber } = req.body;
-  const user = await User.findOneAndUpdate(
-    { _id: req.params.id, isDeleted: false },
-    { username, password, email, contactNumber },
-    { new: true }
-  ).select('-__v');
-  if (!user) return res.status(404).send('User not found or deleted');
-  res.json({ message: 'Updated user', user });
+  try {
+    const { username, email, contactNumber, password } = req.body;
+
+    // Find the user by ID and make sure they are not deleted
+    const user = await User.findOne({ _id: req.params.id, isDeleted: false });
+    if (!user) return res.status(404).json({ message: 'User not found or deleted' });
+
+    // Update fields
+    user.username = username;
+    user.email = email;
+    user.contactNumber = contactNumber;
+    if (password) {
+      user.password = password;
+    }
+
+    // Save the user, triggering the pre('save') middleware
+    await user.save();
+
+    // Remove sensitive data before sending response
+    const userResponse = user.toObject();
+    delete userResponse.password;
+    delete userResponse.__v;
+    delete userResponse.isDeleted;
+
+    res.json({ message: "Updated user with ID: " + user._id, user: userResponse });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
 });
+
 
 router.delete('/:id', verifyToken, async (req, res) => {
   const user = await User.findById(req.params.id);
